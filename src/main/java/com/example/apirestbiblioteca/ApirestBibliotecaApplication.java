9b31348b5e0de57cafa5d131cfe2a8990e11481b
Javier Martinez <javiermarsal11@gmail.com>
2025-01-21 09:38:09 +0100

    init commit

---
+++ src/main/java/com/example/apirestbiblioteca/ApirestBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.example.apirestbiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestBibliotecaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApirestBibliotecaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/apirestbiblioteca/Libro.java
@@ -0,0 +1,71 @@
+package com.example.apirestbiblioteca;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "libro")
+public class Libro {
+    @Id
+    @Size(max = 20)
+    @Column(name = "isbn", nullable = false, length = 20)
+    private String isbn;
+
+    @Size(max = 200)
+    @NotNull
+    @Column(name = "titulo", nullable = false, length = 200)
+    private String titulo;
+
+    @Size(max = 100)
+    @NotNull
+    @Column(name = "autor", nullable = false, length = 100)
+    private String autor;
+
+    public Libro(String isbn, String titulo, String autor) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public Libro() {
+
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "isbn='" + isbn + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                '}';
+    }
+}
+
