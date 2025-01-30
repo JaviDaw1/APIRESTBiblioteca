5bf15f1a2ae35b357fd4bcd6dc717ce8fd749347
Javier Martinez <javiermarsal11@gmail.com>
2025-01-30 11:40:05 +0100

    arreglado validaciones libro

--- src/main/java/com/example/apirestbiblioteca/modelos/Libro.java
+++ src/main/java/com/example/apirestbiblioteca/modelos/Libro.java
@@ -21,21 +21,21 @@ public class Libro {
     @Id
     @Size(max = 20)
     @Column(name = "isbn", nullable = false, length = 20)
-    @Pattern(regexp = "^97[89]-\\d{1,5}-\\d{1,7}-\\d{1,7}-\\d$\n", message = "El isbn debe tener un formato valido")
+    @Pattern(regexp = "^97[89]-\\d{1,5}-\\d{1,7}-\\d{1,7}-\\d$", message = "El isbn debe tener un formato valido")
     private String isbn;
 
     @Size(max = 200)
     @NotNull
     @NotBlank
     @Column(name = "titulo", nullable = false, length = 200)
-    @Pattern(regexp = "^[A-Z][a-zA-Z0-9 ]*$\n", message = "El título debe empezar por letra mayúscula")
+    @Pattern(regexp = "^[A-Z][a-zA-Z0-9 ]*$", message = "El título debe empezar por letra mayúscula")
     private String titulo;
 
     @Size(max = 100)
     @NotNull
     @NotBlank
     @Column(name = "autor", nullable = false, length = 100)
-    @Pattern(regexp = "^[A-Z][a-zA-Z0-9 ]*$\n", message = "El autor debe empezar por mayúscula")
+    @Pattern(regexp = "^[A-Z][a-zA-Z0-9 ]*$", message = "El autor debe empezar por mayúscula")
     private String autor;
 }
 
